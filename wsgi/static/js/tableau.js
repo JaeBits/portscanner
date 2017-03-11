65112743c2fc1d5a746df359e8644aad3298b27e
JaeBits <Jeffreydniemann@gmail.com>
2017-03-11 13:53:57 -0700

    static wsgi

---
+++ wsgi/static/js/tableau.js
@@ -0,0 +1,36 @@
+﻿
+
+////////////////////////////////////////////////////////////////////////////////
+// Global Variables
+
+var dashboardViz, workbook, activeSheet;
+
+////////////////////////////////////////////////////////////////////////////////
+// 1 - Create a View
+
+function initViz(url) {
+
+    var dashboardDiv = document.getElementById("document")
+    dashboardURL = url
+
+    var options = {
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+
+            workbook = dashboardViz.getWorkbook();
+            activeSheet = workbook.getActiveSheet();
+        }
+    };
+
+    dashboardViz = new tableau.Viz(dashboardDiv, dashboardURL, options)
+    dashboardViz.addEventListener(tableauSoftware.TableauEventName.MARKS_SELECTION, onMarksSelection);
+    
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// start scripts
+$(function (){
+    url = $("#url").data("url");
+    initViz(url);
+});
